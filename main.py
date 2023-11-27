6fa7a6e23a6d7a8cdc9b1263548ca85811eb8826
rksharma12 <ritukumarsharma1@gmail.com>
2023-11-27 12:55:45 +0530

    added the code

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import cv2
 import mediapipe as mp
 
-cap=cv2.VideoCapture(0)
-#TODO: Add required mediapipe functions
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+cap = cv2.VideoCapture(0)
 
-while True:
-    success, frame=cap.read()
-    
-    cv2.imshow("Hand Detection",frame)
 
-    #TODO: Implement the functions
+with mp_holistic.Holistic() as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = holistic.process(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    if cv2.waitKey(1) & 0xFF==ord('q'):
-        break
+        if results.right_hand_landmarks:
+            print("right hand")
+            mp_drawing.draw_landmarks(
+                image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        if results.left_hand_landmarks:
+            print("Left Hand")
+            mp_drawing.draw_landmarks(
+                image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
